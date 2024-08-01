f65e4397f75bfe514bdd27e811d94dab9ef1adbe
t4i5ei <astraea.nymph.08@gmail.com>
2024-08-01 23:21:45 +0900

    次回はquizから

--- main.js
+++ main.js
@@ -52,4 +52,30 @@ array = ['Suisei', 'Aqua', 'Towa'];
 //console.log(array[0]); //arrayに入っているSuisei,Aqua,Towaをconsoleに出す場合、出したい文字列等の配列番号を[]にいれる。Suiseiを出力したい場合は0を入力（配列は0スタートだから、1じゃないよ）
 array = []; //配列をすべて書かなくてもいい方法↓array.push('hogehoge(letの中身)');と書くことで行ける
 array.push('Aqua');
-console.log(array);
+
+const coffee = {
+    name: "Cocolate Moka", //hogehoge: "HOGE"前はkey、後ろはvalue 
+    size: 350,
+    isHot: true,
+    toppings: ["Cinnamon", "Caramel"],
+    nutritions: {
+        kcal: 500,
+        suger: 50,
+    },
+}
+//console.log(coffee.size); //coffee.hogehgoeでcoffeeの中にアクセスできる
+coffee.isHot = false //coffeeの中にあるtrueをfalseにしたい時の記述
+coffee.barista = "Mikochi"; //coffeeに新しいプロパティを追加したい時の記述
+
+let userInfo = null; //nullは何も無いということを表す（予定通り何も無いよって時使うらしい）
+userInfo = undefined; //undifinedも何も無いということを表すが、一般的には予期せぬ（エラー的な）ものを表す→ = undifinedは使わない（明示的に何も無いことを表したい時はnullを使用するべき）
+
+function add(num1, num2) { //()の中にパラメーターを入れることができる
+    const value = num1 + num2; //関数の中で定義したものは関数の外側（{}の外）では使えないが、関数の外側で定義されたものは関数の中（スコープ内）で使うことができる（関数の外側、つまりどこでも使える変数をグローバル変数と言い、74行目のvalueの様な変数はローカル変数と言う）
+    console.log(value);
+    //console.log(num1 + num2); //←76行目のadd();が無い状態（要するに74行目を書いただけではconsoleに出力されない
+    return value; //returnが来たタイミングでreture以降の処理はskipされる（今回は79行目までskip→addの内容を取得しているのが79行目だから）
+} //関数の後ろの{}には;（セミコロン）を付けない（ついうかつけるな）
+//add(2, 3); //関数は呼び出す必要がある、呼び出し方は関数名（今回だったらadd）に();　今回はaddを呼び出したいからadd();と書いている。ちなみに何回も呼び出すことができる。
+//↑の時num1には2が入り、num2には3が入る　こんな感じで引数を渡すことができる
+const returnedValue = add(2,3); //←これらを戻り値、返り値と言う
